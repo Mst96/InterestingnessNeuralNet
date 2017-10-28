241063e437d1ca47892543c75561304dbf51c856
Zach <zach_tarbah@hotmail.com>
2017-10-28 11:55:56 +0100

    Added Function to convert Tweet to Matrix

---
+++ textToMat.py
@@ -0,0 +1,18 @@
+import numpy
+
+def textToMat(string) :
+    mat = numpy.zeros((59, 160))
+    for i in range(0,len(string)):
+        ascii = ord(string[i])
+
+        #   a b c ... z = 0 1 2 ... 25
+        if ((ascii > 96) and (ascii < 123)) :
+            j = (ascii-97)
+            mat[j][i] = 1
+
+        #   <spc> ! " to @ = 26 27 28 to 58
+        elif ((ascii > 31) and (ascii < 65)) :
+            j = (ascii-6)
+            mat[j][i] = 1
+
+    return mat
