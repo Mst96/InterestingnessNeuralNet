0ab6edc26cfb703d148a38ae93058748937ddfb6
Tarbah <zach_tarbah@hotmail.com>
2017-11-15 15:42:11 +0000

    Separates Data (csv files already on google drive)

---
+++ separateForTraining.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+PATH = '/Users/zach/documents/uni/t1/scc403/'
+
+trump_tweets_df = pd.read_csv(PATH + 'clean_trump.csv')
+
+trump_tweets_df['split'] = np.random.randn(trump_tweets_df.shape[0], 1)
+
+#Split tweets randomly in ratio 7:3
+msk = np.random.rand(len(trump_tweets_df)) <= 0.7
+
+train = trump_tweets_df[msk]
+test = trump_tweets_df[~msk]
+
+#Run 5 times and change filename to get 5 different sets of data for cross validation
+train.to_csv(PATH + 'training_trump5.csv')
+test.to_csv(PATH + 'testing_trump5.csv')
